9e1ff5eb0176a0afe9a72014c27fe2c9140a8ebc
Kien Nguyen <hoantmhe141105@fpt.edu.vn>
2022-08-09 17:39:47 +0700

    hoa_delete_food

--- src/java/dao/DBContext.java
+++ src/java/dao/DBContext.java
@@ -45,13 +45,9 @@ public abstract class DBContext {
     
        public DBContext() {
         try {
-
-
-            String url = "jdbc:sqlserver://DESKTOP-PV3034R\\SQLEXPRESS:1433;databaseName=FCSchuannhat";
-
-
+             String url = "jdbc:sqlserver://localhost:1433;databaseName=FCS_UPDATE";
             String user = "sa";
-            String pass = "12345";
+            String pass = "123";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
